f79602d5b3369b2372d15e661bbd2eb2c8189cc4
micarossharris15 <mica.ross-harris@and.digital>
2025-04-08 15:33:30 +0100

    Updated content & added back links

--- src/prototypes/summary-focus-order/index.js
+++ src/prototypes/summary-focus-order/index.js
@@ -2,3 +2,5 @@
 Imports question manager, summary generator, answer piping and previous link history
 ************************************************************************************/
 import '@ons/prototype-kit/src/helpers/index.js';
+
+document.getElementById("edit-total-spend").focus();
